edbb4b07bd06e08dd380d736f02f64c446149588
ratomsky <max.work.dev@gmail.com>
2022-06-26 16:55:47 +0300

    feat: add streams

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,104 @@
-import { httpServer } from "./http_server/index";
+import { createWebSocketStream } from "ws";
 import robot from "robotjs";
+
+import { httpServer } from "./http_server/index";
 import { wss } from "./ws_server/index";
+
 import { drawCircle } from "./drawCircle";
 import { capturePrntScrn } from "./capturePrntScrn";
 import { drawRectangle } from "./drawRectangle";
+
+import type { WebSocket } from "ws";
+import internal, { pipeline, Transform } from "stream";
 
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-wss.on("connection", (ws: any) => {
-  ws.on("message", async (message: any) => {
-    const [command, argv1, argv2] = message.toString().split(" ");
-    console.log(command);
+class MyTransform extends Transform {
+  constructor(options: any) {
+    super(options);
+  }
 
-    let { x, y } = robot.getMousePos();
+  override async _transform(
+    chunk: any,
+    _: BufferEncoding,
+    callback: internal.TransformCallback
+  ): Promise<void> {
+    try {
+      const [command, argv1, argv2] = chunk.toString().split(" ");
+      let res: string = command;
+      console.log(command);
 
-    if (command === "mouse_left") {
-      robot.moveMouse(x - Number(argv1), y);
-    }
-    if (command === "mouse_down") {
-      robot.moveMouse(x, y + Number(argv1));
-    }
-    if (command === "mouse_right") {
-      robot.moveMouse(x + Number(argv1), y);
-    }
-    if (command === "mouse_up") {
-      robot.moveMouse(x, y - Number(argv1));
-    }
+      let { x, y } = robot.getMousePos();
 
-    if (command === "draw_circle") {
-      drawCircle(argv1);
-    }
+      if (command === "mouse_left") {
+        robot.moveMouse(x - Number(argv1), y);
+      }
+      if (command === "mouse_down") {
+        robot.moveMouse(x, y + Number(argv1));
+      }
+      if (command === "mouse_right") {
+        robot.moveMouse(x + Number(argv1), y);
+      }
+      if (command === "mouse_up") {
+        robot.moveMouse(x, y - Number(argv1));
+      }
 
-    if (command === "draw_square") {
-      drawRectangle(argv1);
-    }
+      if (command === "draw_circle") {
+        drawCircle(argv1);
+      }
 
-    if (command === "draw_rectangle") {
-      drawRectangle(argv1, argv2);
-    }
+      if (command === "draw_square") {
+        drawRectangle(argv1);
+      }
 
-    if (command === "mouse_position") {
-      ws.send(`mouse_position ${x},${y}`);
-    }
+      if (command === "draw_rectangle") {
+        drawRectangle(argv1, argv2);
+      }
 
-    if (command === "prnt_scrn") {
-      const base64_img = await capturePrntScrn({ x, y, h: 200, w: 200 });
-      ws.send(`prnt_scrn ${base64_img}`);
-    }
-  });
-});
+      if (command === "mouse_position") {
+        res = `mouse_position ${x},${y}`;
+      }
 
-wss.on("close", () => {
-  console.log("socket closed");
-});
+      if (command === "prnt_scrn") {
+        const base64_img = await capturePrntScrn({ x, y, h: 200, w: 200 });
+        res = `prnt_scrn ${base64_img}`;
+      }
+
+      callback(null, `${res}\0`);
+    } catch (error) {
+      callback(error as Error);
+    }
+  }
+}
+
+try {
+  wss
+    .on("connection", async (socket: WebSocket) => {
+      const stream = createWebSocketStream(socket, {
+        decodeStrings: false,
+        encoding: "utf8",
+        allowHalfOpen: false,
+      });
+      const transform = new MyTransform({
+        decodeStrings: false,
+        encoding: "utf8",
+        allowHalfOpen: false,
+      });
+
+      pipeline(stream, transform, stream, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+
+      socket.on("close", () => stream.destroy());
+    })
+    .on("close", () => {
+      console.log("Socket closed");
+    });
+} catch (error) {
+  console.error("error", error);
+}
